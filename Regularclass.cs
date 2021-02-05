320fd1b44d02ffc2e89a69f1d6f91fa7d22639f2
BlancaLujan <77300324+BlancaLujan@users.noreply.github.com>
2021-02-04 19:36:58 -0700

    Update and rename Program.cs to Regularclass.cs
    Regular class with a generic method

---
+++ Regularclass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Program.File.MyClasses;
+
+
+
+namespace Program.File
+{
+    class Programs
+    {
+        static void Main()
+        {
+            UserClass myNewClass = new UserClass();
+
+            myNewClass.PrintSomethingToTheConsole<string, string>(1, "User1" , "Administrator");
+
+        }
+    }
+}
+public class UserClass
+{
+    public void PrintSomethingToTheConsole<T, Key>(int UserId, Key UserName, T UserRole)
+    {
+        Console.WriteLine("The value of the parameters are the following:");
+        Console.WriteLine(UserId);
+        Console.WriteLine(UserName);
+        Console.WriteLine(UserRole);
+    }
+}
+
+
