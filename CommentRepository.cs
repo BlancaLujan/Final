40aec6a1efc6c82c5a5f6ee9952f327525644ed2
BlancaLujan <77300324+BlancaLujan@users.noreply.github.com>
2021-02-10 18:21:28 -0700

    Add files via upload

---
+++ CommentRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Program.File.MyClasses;
+using Program.File.Repositories.Implementation;
+
+namespace Program.File.Repositories
+{
+    public class CommentRepository : MainRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository()
+        {
+            List<Comment> resultList =
+              currentDataBase.Where(comment => comment.Comments == "").ToList();
+            if (resultList.Count > 0)
+            {
+                foreach (var user in resultList)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No comment found");
+            }
+        }
+
+        public void GetAllComments(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Program.File.Expections;
+using Program.File.MyClasses;
+
+namespace Program.File.Repositories.Implementation
+{
+    public class UserRepository : MainRepository<User>, IUser_Repository
+    {
+        public UserRepository()
+        {
+
+            List<User> resultList =
+                currentDataBase.Where(user => user.UserName == "Blanca").ToList();
+
+            if (resultList.Count > 0)
+            {
+                foreach (var user in resultList)
+                {
+                    Console.WriteLine(user.UserName);
+                    Console.WriteLine(user.UserId);
+                    Console.WriteLine(user.UserRole);
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No user found");
+            }
+        }
+
+        public void GetAllUserNames(string name)
+        {
+            Console.WriteLine();
+
+
+        }
+
+        internal void Save(Comment newComment)
+        {
+            Console.WriteLine();
+        }
+
+        internal void Delete(Comment newComment2)
+        {
+            Console.WriteLine();
+        }
+    }
+}
